2e7f5e205a88eed3490ccd7bc34b018dbf0c9371
Matthew Daubney <mdaubney@arcapix.com>
2024-02-21 14:32:18 +0000

    Update pydantic

--- flask_pydantic_spec/security_components.py
+++ flask_pydantic_spec/security_components.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field
 class SecurityScheme(BaseModel):
     type: Literal["http", "apiKey"]
     scheme: Optional[Literal["Basic", "Bearer"]]
-    _in: Optional[Literal["header", "query", "cookie"]] = Field(serialization_alias="in")
+    found_in: Optional[Literal["header", "query", "cookie"]] = Field(serialization_alias="in")
     bearerFormat: Optional[str]
     name: str
     description: Optional[str]
