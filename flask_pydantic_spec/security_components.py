e0b47f7ec566ed2bef6fff556e2c3a50bfbe8843
Matthew Daubney <mdaubney@arcapix.com>
2024-02-21 14:18:59 +0000

    Alias a field name

--- flask_pydantic_spec/security_components.py
+++ flask_pydantic_spec/security_components.py
@@ -1,12 +1,12 @@
 from typing import Literal, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class SecurityScheme(BaseModel):
     type: Literal["http", "apiKey"]
     scheme: Optional[Literal["Basic", "Bearer"]]
-    _in: Optional[Literal["header", "query", "cookie"]]
+    _in: Optional[Literal["header", "query", "cookie"]] = Field(serialization_alias="in")
     bearerFormat: Optional[str]
     name: str
     description: Optional[str]
